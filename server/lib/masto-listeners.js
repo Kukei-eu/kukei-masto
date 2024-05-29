eceb3c87c627c787168fa62228611621b9c7cfc3
Szymon Nowicki <hey@nowicki.io>
2024-05-29 15:57:39 +0200

    feat: add more sources

--- server/lib/masto-listeners.js
+++ server/lib/masto-listeners.js
@@ -31,8 +31,16 @@ export const startListening = () => {
 	const polSocial = makeListener(new MastoApi('pol.social'));
 	const dziesiony = makeListener(new MastoApi('101010.pl'));
 	const mastodonSocial = makeListener(new MastoApi('mastodon.social'));
+	const mastodonOnline = makeListener(new MastoApi('mastodon.online'));
+	const fosstodonOrg = makeListener(new MastoApi('fosstodon.org'));
+	const grueneSocial = makeListener(new MastoApi('gruene.social'));
+
 	listeners.push(polSocial);
 	listeners.push(dziesiony);
 	listeners.push(mastodonSocial);
+	listeners.push(mastodonOnline);
+	listeners.push(fosstodonOrg);
+	listeners.push(grueneSocial);
+
 	run();
 }
