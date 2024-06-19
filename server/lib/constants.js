63c15489567fd0bed9b4f6f2c94d86af934d7aea
Szymon Nowicki <sznowicki@eskalacja.com>
2024-06-19 22:34:56 +0200

    feat: bump ttl to 6 hours

--- server/lib/constants.js
+++ server/lib/constants.js
@@ -1,3 +1,3 @@
-export const TOOTS_TTL_MS = (1000 * 60 * 60) * 4;
+export const TOOTS_TTL_MS = (1000 * 60 * 60) * 6;
 
-export const TOOTS_TTL_HUMAN = '4 hours';
+export const TOOTS_TTL_HUMAN = '6 hours';
