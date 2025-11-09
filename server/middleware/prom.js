65e65b415f4679a7210cd7c604b7e6b43684578a
Szymo Nowicki <hey@nowicki.io>
2025-11-09 15:59:40 +0100

    feat: enable prometheus

--- server/middleware/prom.js
+++ server/middleware/prom.js
@@ -1,5 +1,18 @@
+import client from 'prom-client';
 import promBundle from 'express-prom-bundle';
-const metricsMiddleware = promBundle({includeMethod: true});
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+const prefix = 'kukei_masto_';
+collectDefaultMetrics({ prefix });
+
+
+export const promClient = client;
+
+const metricsMiddleware = promBundle({
+	includeMethod: true,
+	includePath: true,
+	promClient
+});
 
 export const makePromMiddleware = (app) => {
 	app.use(metricsMiddleware);
